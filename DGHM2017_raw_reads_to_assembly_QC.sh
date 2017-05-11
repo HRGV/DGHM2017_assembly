d517b8ffd087f02b3b5cc63f12539a7859925ea1
HRGV <hgruber@mpi-bremen.de>
2017-05-11 11:35:01 +0200

    add kaiju

--- DGHM2017_raw_reads_to_assembly_QC.sh
+++ DGHM2017_raw_reads_to_assembly_QC.sh
@@ -8,10 +8,13 @@ for n in 1 2; do wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR076/DRR076717/DRR076
 #run read level qualtiy control
 #fix - download fastqc and call from dl file!
 #for n in *fastq.gz; do fastqc $n; done FIX
+#run kaiju for taxonomic composition screening
+kaiju -f ~/save/kaiju_db.fmi -t ~/save/nodes.dmp -i <(zcat DRR076717_1.fastq.gz | head -n 400000) -j <(zcat DRR076717_2.fastq.gz | head -n 400000) -a greedy -e 5 -o DRR076717.100k.kaiju.out.txt
+kaijuReport -n ~/save/names.dmp -t ~/save/nodes.dmp -i DRR076717.100k.kaiju.out.txt -o DRR076717.100k.kaiju.out.genus.txt -r genus -m .1
 #optional - SSU based library quality and composition tests
 #phyloFlash_latest -lib DRR076717 -read1 DRR076717_1.fastq.gz -read2 DRR076717_2.fastq.gz -readlength 150 -skip_spades -skip_emirge
 #adapter trimming from the read end
-bbduk.sh ref=/opt/bbmap/resources/adapters.fa interleaved=t ktrim=r trimq=2 qtrim=rl minlength=50 mink=11 hdist=1 in=DRR076717_1.fastq.gz in2=DRR076717_2.fastq.gz out=DRR076717_all_fr_ktrimr_q2.fq.gz overwrite=t
+bbduk.sh ref=/opt/bbmap/resources/adapters.fa interleaved=t ktrim=r trimq=2 qtrim=rl minlength=50 mink=11 hdist=1 in=DRR076717_1.fastq.gz in2=DRR076717_2.fastq.gz out=DRR076717_all_fr_ktrimr_q2.fq.gz overwrite=t reads=100000 #reads=-1 for all reads or remove switch completely
 #run kmer count based ultra low coverage read removal
 bbnorm.sh in=DRR076717_all_fr_ktrimr_q2.fq.gz interleaved=t hist=DRR076717_hist.txt peaks=DRR076717_peaks.txt -Xmx10g lowbindepth=1 highbindepth=3 outhigh=DRR076717_kmerfilt3_fr.fq.gz passes=1
 #check the input files again using fastqc
